1dcda18254e478d8d03f753f6c4cf62b3a3fe0c3
Roberto <Roberto@DESKTOP-70OPO22>
2016-01-11 22:25:56 -0500

    Cambio en los controladores...

--- ProjectSiges/src/java/com/entity/Facultad.java
+++ ProjectSiges/src/java/com/entity/Facultad.java
@@ -5,93 +5,109 @@
 package com.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author DARIO
+ * @author Roberto
  */
 @Entity
 @Table(name = "facultad")
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "Facultad.findAll", query = "SELECT f FROM Facultad f"),
-    //@NamedQuery(name = "Facultad.findByCodigoFacultad", query = "SELECT f FROM Facultad f WHERE f.codigoFacultad = :codigoFacultad"),
-    @NamedQuery(name = "Facultad.findByTipoFacultad", query = "SELECT f FROM Facultad f WHERE f.tipoFacultad = :tipoFacultad")})
+    @NamedQuery(name = "Facultad.findByIdFacultad", query = "SELECT f FROM Facultad f WHERE f.idFacultad = :idFacultad"),
+    @NamedQuery(name = "Facultad.findByNombreFacultad", query = "SELECT f FROM Facultad f WHERE f.nombreFacultad = :nombreFacultad")})
 public class Facultad implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    /*@GeneratedValue(strategy = GenerationType.IDENTITY)
-     @Basic(optional = false)
-     @Column(name = "codigo_Facultad")
-     private Integer codigoFacultad;*/
     @Id
-    @ManyToOne(cascade = CascadeType.MERGE)
-    @JoinColumn(name = "codigo_Profesor", nullable = false)
-    private Profesor codigoProfesor;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID_FACULTAD")
+    private Integer idFacultad;
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 40)
-    @Column(name = "tipoFacultad")
-    private String tipoFacultad;
+    @Size(min = 1, max = 100)
+    @Column(name = "NOMBRE_FACULTAD")
+    private String nombreFacultad;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idFacultad")
+    private Collection<Profesor> profesorCollection;
 
     public Facultad() {
     }
 
-    public String getTipoFacultad() {
-        return tipoFacultad;
+    public Facultad(Integer idFacultad) {
+        this.idFacultad = idFacultad;
     }
 
-    public void setTipoFacultad(String tipoFacultad) {
-        this.tipoFacultad = tipoFacultad;
+    public Facultad(Integer idFacultad, String nombreFacultad) {
+        this.idFacultad = idFacultad;
+        this.nombreFacultad = nombreFacultad;
     }
 
-    public Profesor getCodigoProfesor() {
-        return codigoProfesor;
+    public Integer getIdFacultad() {
+        return this.idFacultad;
     }
 
-    public void setCodigoProfesor(Profesor codigoProfesor) {
-        this.codigoProfesor = codigoProfesor;
+    public void setIdFacultad(Integer idFacultad) {
+        this.idFacultad = idFacultad;
     }
-    /*
-     @Override
-     public int hashCode() {
-     int hash = 0;
-     hash += (codigoFacultad != null ? codigoFacultad.hashCode() : 0);
-     return hash;
-     }
 
-     @Override
-     public boolean equals(Object object) {
-     // TODO: Warning - this method won't work in the case the id fields are not set
-     if (!(object instanceof Facultad)) {
-     return false;
-     }
-     Facultad other = (Facultad) object;
-     if ((this.codigoFacultad == null && other.codigoFacultad != null) || (this.codigoFacultad != null && !this.codigoFacultad.equals(other.codigoFacultad))) {
-     return false;
-     }
-     return true;
-     }
+    public String getNombreFacultad() {
+        return nombreFacultad;
+    }
 
-     @Override
-     public String toString() {
-     return "com.entity.Facultad[ codigoFacultad=" + codigoFacultad + " ]";
-     }*/
+    public void setNombreFacultad(String nombreFacultad) {
+        this.nombreFacultad = nombreFacultad;
+    }
 
-    public void setCodigoProfesor() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    @XmlTransient
+    public Collection<Profesor> getProfesorCollection() {
+        return profesorCollection;
+    }
+
+    public void setProfesorCollection(Collection<Profesor> profesorCollection) {
+        this.profesorCollection = profesorCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idFacultad != null ? idFacultad.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Facultad)) {
+            return false;
+        }
+        Facultad other = (Facultad) object;
+        if ((this.idFacultad == null && other.idFacultad != null) || (this.idFacultad != null && !this.idFacultad.equals(other.idFacultad))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.entity.Facultad[ idFacultad=" + idFacultad + " ]";
     }
 }
