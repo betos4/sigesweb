42b2da6d2db983ee1a5254ddfa4e57d7ae7c04d2
Roberto <Roberto@DESKTOP-70OPO22>
2016-01-11 23:36:52 -0500

    Nuevo intento

--- ProjectSiges/build/generated-sources/ap-source-output/com/entity/Facultad_.java
+++ ProjectSiges/build/generated-sources/ap-source-output/com/entity/Facultad_.java
@@ -2,14 +2,16 @@ package com.entity;
 
 import com.entity.Profesor;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2016-01-10T11:12:50")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2016-01-11T23:27:42")
 @StaticMetamodel(Facultad.class)
 public class Facultad_ { 
 
-    public static volatile SingularAttribute<Facultad, Profesor> codigoProfesor;
-    public static volatile SingularAttribute<Facultad, String> tipoFacultad;
+    public static volatile SingularAttribute<Facultad, String> nombreFacultad;
+    public static volatile CollectionAttribute<Facultad, Profesor> profesorCollection;
+    public static volatile SingularAttribute<Facultad, Integer> idFacultad;
 
 }
--- ProjectSiges/build/generated-sources/ap-source-output/com/entity/Profesor_.java
+++ ProjectSiges/build/generated-sources/ap-source-output/com/entity/Profesor_.java
@@ -1,22 +1,20 @@
 package com.entity;
 
-import java.util.Date;
+import com.entity.Facultad;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2016-01-10T11:12:50")
+@Generated(value="EclipseLink-2.5.0.v20130507-rNA", date="2016-01-11T23:27:42")
 @StaticMetamodel(Profesor.class)
 public class Profesor_ { 
 
-    public static volatile SingularAttribute<Profesor, String> direccion;
-    public static volatile SingularAttribute<Profesor, String> nombres;
-    public static volatile SingularAttribute<Profesor, Integer> codigoProfesor;
-    public static volatile SingularAttribute<Profesor, String> apellidoPaterno;
-    public static volatile SingularAttribute<Profesor, String> apellidoMaterno;
-    public static volatile SingularAttribute<Profesor, String> sexo;
-    public static volatile SingularAttribute<Profesor, Date> fechaNacimiento;
-    public static volatile SingularAttribute<Profesor, Integer> cedula;
-    public static volatile SingularAttribute<Profesor, String> correo;
+    public static volatile SingularAttribute<Profesor, Integer> celularProfesor;
+    public static volatile SingularAttribute<Profesor, String> correoProfesor;
+    public static volatile SingularAttribute<Profesor, Facultad> idFacultad;
+    public static volatile SingularAttribute<Profesor, Long> cedulaProfesor;
+    public static volatile SingularAttribute<Profesor, String> apellidoProfesor;
+    public static volatile SingularAttribute<Profesor, String> nombreProfesor;
+    public static volatile SingularAttribute<Profesor, Integer> telefonoProfesor;
 
 }
