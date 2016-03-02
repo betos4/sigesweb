86e154a769b618e2eef1ae929cb2e45c88fd3b7c
Roberto <Roberto@DESKTOP-70OPO22>
2016-03-01 22:27:15 -0500

    Modificacion en Modulos

--- ProjectSiges/src/java/com/bean/Redireccion.java
+++ ProjectSiges/src/java/com/bean/Redireccion.java
@@ -27,36 +27,13 @@ public class Redireccion {
         }
     }
     
-    public void ingresarProfesor() {
-        try {
-            FacesContext
-                    .getCurrentInstance()
-                    .getExternalContext()
-                    .redirect("/ProjectSiges/pages/gestionAdministracion/profesor/listarProfesorIngreso.xhtml");
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
-
-    //Esto es para Facultad
-    public void ingresarFacultad() {
-        try {
-            FacesContext
-                    .getCurrentInstance()
-                    .getExternalContext()
-                    .redirect("/ProjectSiges/pages/gestionAdministracion/facultad/listarFacultadIngreso.xhtml");
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
-
     /***************************************Para el boton que regresa a los modulos**************************************/
     public void atrasModulos() {
         try {
             FacesContext
                     .getCurrentInstance()
                     .getExternalContext()
-                    .redirect("/ProjectSiges/pages/gestionAdministracion/modulosAdministracion.xhtml");
+                    .redirect("/ProjectSiges/modulo.xhtml");
         } catch (Exception e) {
             // TODO: handle exception
         }
